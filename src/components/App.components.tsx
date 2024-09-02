60828e9b7aaf728ffb9974349e04738632a2fba3
AlexBatya <alex28.12.12@mail.ru>
2024-09-02 08:35:09 +0300

    'new'

--- src/components/App.components.tsx
+++ src/components/App.components.tsx
@@ -22,113 +22,116 @@ function App() {
   const { id } = useParams<{ id: string }>();
 
   useEffect(() => {
-		if (document.readyState === "complete") {
-			setIsLoading(false);
-		} else {
-			const handlePageLoad = () => {
-				setIsLoading(false);
-			};
+    // Проверяем, если страница уже загружена
+    if (document.readyState === "complete") {
+      setIsLoading(false);
+    } else {
+      // Добавляем слушатель на событие загрузки страницы
+      const handlePageLoad = () => {
+        setIsLoading(false);
+      };
 
-			window.addEventListener('load', handlePageLoad);
+      window.addEventListener('load', handlePageLoad);
 
-			return () => {
-				window.removeEventListener('load', handlePageLoad);
-			};
-		}
+      // Очищаем слушатель при размонтировании компонента
+      return () => {
+        window.removeEventListener('load', handlePageLoad);
+      };
+    }
   }, []);
 
   const handleGuestsChange = (newGuests: GuestType[]) => {
-		setGuests(newGuests);
-		setResetGuestCountSignal(false); // Сбрасываем сигнал сброса, когда гости изменяются
+    setGuests(newGuests);
+    setResetGuestCountSignal(false); // Сбрасываем сигнал сброса, когда гости изменяются
   };
 
   const handleConfirm = async (confirmedGuests: GuestType[]) => {
-		try {
-			for (const guest of confirmedGuests) {
-				await GuestService.addGuest(guest);
-			}
-			setGuests([]);
-			setShouldContinueRight(true);
-			setIsSuccessPopupOpen(true);
-		} catch (err) {
-			console.error('Ошибка при добавлении гостей:', err);
-		}
+    try {
+      for (const guest of confirmedGuests) {
+        await GuestService.addGuest(guest);
+      }
+      setGuests([]);
+      setShouldContinueRight(true);
+      setIsSuccessPopupOpen(true);
+    } catch (err) {
+      console.error('Ошибка при добавлении гостей:', err);
+    }
   };
 
   const handleCancel = () => {
-		setGuests([]);
-		setShouldContinueRight(false);
-		setResetGuestCountSignal(true); // Устанавливаем сигнал для сброса гостей
+    setGuests([]);
+    setShouldContinueRight(false);
+    setResetGuestCountSignal(true); // Устанавливаем сигнал для сброса гостей
   };
 
   const handleCloseSuccessPopup = () => {
-		setIsSuccessPopupOpen(false);
+    setIsSuccessPopupOpen(false);
   };
 
   const style: React.CSSProperties = {
-		width: "95vw",
-		overflow: "hidden",
-		display: "flex",
-		alignItems: 'center',
-		flexDirection: 'column',
-		gap: "10px",
-		fontFamily: "CormorantInfant-Bold"
+    width: "95vw",
+    overflow: "hidden",
+    display: "flex",
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: "10px",
+    fontFamily: "CormorantInfant-Bold"
   };
 
   const backStyle: React.CSSProperties = {
-		width: "400px",
-		height: "40px",
-		backgroundColor: "rgba(0, 0, 0, 0.05)",
-		backgroundImage: "url(" + back + ")",
-		backgroundSize: "cover",
-		backgroundPosition: "center",
-		borderRadius: "5px",
+    width: "400px",
+    height: "40px",
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
+    backgroundImage: "url(" + back + ")",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    borderRadius: "5px",
   };
 
   const back_img: React.CSSProperties = {
-		padding: "20px 0",
-		width: "400px",
-		height: "100%",
-		display: 'flex',
-		flexDirection: "column",
-		alignItems: 'center',
-		justifyContent: "center",
-		backgroundImage: "url(" + back + ")",
-		backgroundRepeat: "no-repeat",
-		backgroundColor: "rgba(0, 0, 0, 0.009)",
-		backgroundSize: "cover",
-		backgroundPosition: "center",
+    padding: "20px 0",
+    width: "400px",
+    height: "100%",
+    display: 'flex',
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: "center",
+    backgroundImage: "url(" + back + ")",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "rgba(0, 0, 0, 0.009)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   };
 
   return (
-		<>
-			{isLoading ? (
-				<Loader />
-			) : (
-				<div style={style}>
-					<Heart />
-					<Time />
-					<div style={back_img}>
-						<div style={backStyle}></div>
-						<Guest id={Number(id)} />
-						<NewGuests 
-							onGuestsChange={handleGuestsChange}
-							resetGuestCountSignal={resetGuestCountSignal} // Передаем сигнал сброса
-						/>
-						<SwipeButton 
-							guests={guests} 
-							onConfirm={handleConfirm} 
-							onCancel={handleCancel} 
-							shouldContinueRight={shouldContinueRight}
-							id = {Number(id)}
-						/>
-						<p style={{ userSelect: "none", padding: "20px 0" }}>Мы рады видеть вас на нашей свадьбе</p>
-						<div style={backStyle}></div>
-					</div>
-				</div>
-			)}
-			<SuccessPopup isOpen={isSuccessPopupOpen} onClose={handleCloseSuccessPopup} />
-		</>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div style={style}>
+          <Heart />
+          <Time />
+          <div style={back_img}>
+            <div style={backStyle}></div>
+            <Guest id={Number(id)} />
+            <NewGuests 
+              onGuestsChange={handleGuestsChange}
+              resetGuestCountSignal={resetGuestCountSignal} // Передаем сигнал сброса
+            />
+            <SwipeButton 
+              guests={guests} 
+              onConfirm={handleConfirm} 
+              onCancel={handleCancel} 
+              shouldContinueRight={shouldContinueRight}
+              id={Number(id)}
+            />
+            <p style={{ userSelect: "none", padding: "20px 0" }}>Мы рады видеть вас на нашей свадьбе</p>
+            <div style={backStyle}></div>
+          </div>
+        </div>
+      )}
+      <SuccessPopup isOpen={isSuccessPopupOpen} onClose={handleCloseSuccessPopup} />
+    </>
   );
 }
 
